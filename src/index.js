b8e3ef0ffe85fd9ee83bb07357a9dd0a7106553d
crapthings <crapthings@gmail.com>
2019-08-07 09:49:19 +0800

    minor update

--- src/index.js
+++ src/index.js
@@ -22,20 +22,6 @@ const withWormhole = componentName => {
 
       universe[componentName] = this
 
-      this.wormhole = name => {
-        if (Object.prototype.toString.call(name) === '[object String]') {
-          return universe[name]
-        } else if (Object.prototype.toString.call(name) === '[object Array]') {
-          const ref = {}
-          for (const _name of name) {
-            ref[_name] = universe[_name]
-          }
-          return ref
-        } else {
-          return universe
-        }
-      }
-
       if (componentDidMount) {
         componentDidMount.apply(this, arguments)
       }
@@ -45,6 +31,20 @@ const withWormhole = componentName => {
       delete universe[componentName]
       if (componentWillUnmount) {
         componentWillUnmount.apply(this, arguments)
+      }
+    }
+
+    component.prototype.wormhole = function (name) {
+      if (Object.prototype.toString.call(name) === '[object String]') {
+        return universe[name]
+      } else if (Object.prototype.toString.call(name) === '[object Array]') {
+        const ref = {}
+        for (const _name of name) {
+          ref[_name] = universe[_name]
+        }
+        return ref
+      } else {
+        return universe
       }
     }
 
